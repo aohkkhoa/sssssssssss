a24ebed37a4d4a9e90420356c07ca1699762e1bd
aohkkhoa <78409538+aohkkhoa@users.noreply.github.com>
2021-03-17 17:16:12 +0700

    uploadfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileService } from './file.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularfile1';
+  public images : any = []
+  constructor(private fileservice : FileService){}
+  ngOnInit(){
+    this.fileservice.getImages().subscribe(data => {
+       this.images = data 
+    });
+  }
 }
